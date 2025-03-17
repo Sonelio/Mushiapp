cb7a7eeab08520650d9c8bfad0a003f41eeaeb7c
Sonelio <vaiciunas.sonis@gmail.com>
2025-03-17 13:37:26 +0200

    update page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,4 @@ import { redirect } from 'next/navigation';
 export default function HomePage() {
   // This will redirect on the server side
   redirect('/auth/login');
-  
-  // This fallback will never be rendered
-  return null;
 } 
